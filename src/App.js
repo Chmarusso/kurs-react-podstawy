bf9daac4564e93d441a232ba57eff252f6270939
Artur Chmaro <artur.chmaro@consileon.pl>
2018-08-23 18:57:30 +0200

    Adding elements to state

--- src/App.js
+++ src/App.js
@@ -1,20 +1,47 @@
 import React, { Component } from 'react';
 
 class ToDoList extends Component {
+  state = {
+    tasks: this.props.tasks,
+    draft: ''
+  }
+
+  updateDraft = event => {
+    this.setState({draft: event.target.value})
+  }
+
+  addToDo = () => {
+    const { tasks, draft } = this.state
+    const list = tasks
+    list.push(draft)
+    this.setState({tasks: list, draft: ''})
+  }
+
   render() {
+    const { title } = this.props
+    const { tasks, draft } = this.state
+
     return (
       <div>
-        <h1>{this.props.title}</h1>
+        <h1>{title}</h1>
+        {tasks.map(task => <div><p>{task}</p></div>)}
+        <input type='text' onChange={this.updateDraft} value={draft} />
+        <button onClick={this.addToDo}>Add</button>
       </div>
     )
   }
 }
 
 class App extends Component {
+  myTasks = [
+    'Record a ReactJS video',
+    'Go for a walk'
+  ]
+
   render() {
     return (
       <div>
-        <ToDoList title='My stuff' />
+        <ToDoList title='My stuff' tasks={this.myTasks} />
       </div>
     );
   }
